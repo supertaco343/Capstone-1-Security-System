45aa9fdb062a223173b2400ad0a2b9d4761a9e32
bmgb5h <bmgb5h@mst.edu>
2024-10-31 12:25:29 -0500

    add a testing folder

---
+++ tests/test_box_model.py
@@ -0,0 +1,19 @@
+import sys
+import os
+
+# Add the parent directory to the path so that we can import the model module
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+from model import Model
+
+BASE_DIR = os.path.dirname(__file__)
+
+MODEL_PATH = os.path.join(BASE_DIR, "..", "models", "box_model.pt")
+IMG_PATH = os.path.join(BASE_DIR, "images", "test.jpg")
+
+box_model = Model(None, MODEL_PATH)
+
+box_model.predict(IMG_PATH)
+
+for result in box_model.results:
+    result.show()
