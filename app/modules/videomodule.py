a0a3f721db3d0548060a84fb0b78702bb312a907
supertaco343 <supertaco343@gmail.com>
2024-11-12 18:48:23 -0500

    Found and fixed issue (note in changelog)

--- app/modules/videomodule.py
+++ app/modules/videomodule.py
@@ -1,22 +1,26 @@
 import cv2 #used to use the camera(s)
 
 def CameraRecord(CamID, TempPath, TempType, Resolution, FrameRate, TimeLimit):
-    CamID = 0 #HARDCODED BECAUSE CV2 DOESNT LIKE DEVICE ID, WILL BE FIXED LATER ON
+    CamID = 2 #HARDCODED BECAUSE CV2 DOESNT LIKE DEVICE ID, WILL BE FIXED LATER ON
     
     cv2.namedWindow("Test")
     cam = cv2.VideoCapture(CamID)
-
-    Res = [Resolution.split("x")[0], Resolution.split("x")[1]]
+    
+    cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(TempType[0], TempType[1], TempType[2], TempType[3]))
+    cam.set(cv2.CAP_PROP_FPS, int(FrameRate))
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+    
+    Res = (int(Resolution.split("x")[0]), int(Resolution.split("x")[1]))
     print(Res[0], Res[1])
     frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
     frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    print(frame_width, frame_height)
+    print(frame_width, frame_height) #this is to check to make sure it is the correct size (prints what is in the config, and what is currently set)
     
     # Define the codec and create VideoWriter object
     print(TempType[0], TempType[1], TempType[2], TempType[3])
     fourcc = cv2.VideoWriter_fourcc(TempType[0], TempType[1], TempType[2], TempType[3])
-    #out = cv2.VideoWriter('output.mp4', fourcc, int(FrameRate), (int(Res[0]), int(Res[1])))
-    out = cv2.VideoWriter('output.mp4', fourcc, 5, (640, 480))
+    out = cv2.VideoWriter('output.mjpg', fourcc, int(FrameRate), (Res[0], Res[1]))
     
     if cam.isOpened():
         rval, frame = cam.read()
@@ -26,7 +30,7 @@ def CameraRecord(CamID, TempPath, TempType, Resolution, FrameRate, TimeLimit):
     print(rval)
     
     while rval:
-        print("here")
+        #print("here")
         rval, frame = cam.read()
 
         # Write the frame to the output file
